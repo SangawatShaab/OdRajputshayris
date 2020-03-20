4f02d206a29ff827b8aeb901d612f483c198b21e
SangawatShaab <shubhamr1414@gmail.com>
2020-03-21 01:06:12 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("nav");
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
